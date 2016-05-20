a850e9f0d702b730b29f3887ebb60dcc57a3d249
James <jharrison23@knights.ucf.edu>
2016-05-20 13:11:57 -0400

    Initial Commit

---
+++ Maps/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Maps
+//
+//  Created by James Harrison on 5/20/16.
+//  Copyright © 2016 James Harrison. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
